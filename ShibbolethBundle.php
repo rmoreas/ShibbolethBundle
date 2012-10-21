5ae5ca92fd87556ea711ecc131ee98a574555668
Ronny Moreas <ronny.moreas@mech.kuleuven.be>
2012-10-21 17:07:43 +0200

    Moved registration of ShibbolethFactory from configuration to build the method of the ShibbolethBundle

--- ShibbolethBundle.php
+++ ShibbolethBundle.php
@@ -3,7 +3,16 @@
 namespace KULeuven\ShibbolethBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use KULeuven\ShibbolethBundle\DependencyInjection\Security\ShibbolethFactory;
 
 class ShibbolethBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new ShibbolethFactory());
+    }    
 }
